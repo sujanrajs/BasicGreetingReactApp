52f549b242af13ec5cc73d2dcec12d7781144087
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-05-03 22:28:30 +0300

    using appjs

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  let curDate = new Date();
+  curDate = curDate.getHours();
+  let greeting = "";
+
+  const cssStyle = {};
+
+  if (curDate >= 1 && curDate < 12) {
+    greeting = "Good Morning!";
+    cssStyle.color = "green";
+  } else if (curDate >= 12 && curDate < 19) {
+    greeting = "Good Afternoon!";
+    cssStyle.color = "orange";
+  } else {
+    greeting = "Good Night!";
+    cssStyle.color = "black";
+  }
+
+  return (
+    <>
+      <div>
+        <h1>
+          {" "}
+          Hello user, <span style={cssStyle}>{greeting}</span>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
